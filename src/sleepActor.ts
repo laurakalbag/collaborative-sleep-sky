fc4206338e5271971b0bd3e6533c043038325693
laurakalbag <me@laurakalbag.com>
2023-11-06 15:38:44 +0000

    Working demo!

--- src/sleepActor.ts
+++ src/sleepActor.ts
@@ -1,29 +1,18 @@
-import { createActor, fromCallback } from 'xstate';
 import { actorFromStately } from '@statelyai/sky';
 import { v4 as uuidv4 } from 'uuid';
-import { skyConfig } from './sleepActor.sky';
+import { createActor, fromCallback } from 'xstate';
 import { starGraphic } from './graphics';
+import { skyConfig } from './sleepActor.sky';
 import './styles.css';
-
-let session: string = uuidv4();
-// console.log('Session:', session);
-
-const actor = actorFromStately(
-  {
-    url: 'https://sky.stately.ai/YKYFNR',
-    sessionId: session,
-  },
-  skyConfig,
-);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div id="starscape"></div>
 <div id="wrapper">
   <div id="content">
     <h1>Collaborative sleep</h1>
-    <p>There are <span id="star-number">0</span> stars online.</p>
+    <p>There <span id="star-number">are 0 stars</span> online.</p>
     <ul id="buttons">
-      <li><button id="sound-toggle">Sound off</button></li>
+      <li><button id="sound-toggle">Turn sound on</button></li>
     </ul>
   </div>
   <audio id="audio-player" loop>
@@ -34,10 +23,89 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 </div>
 `;
 
-const starscape = document.querySelector('#starscape') as any;
+const starscape = document.querySelector('#starscape') as Element;
+const starmap: Map<string, { x: number; y: number }> = new Map();
+
+// Add stars
+
+function starCoOrdinates() {
+  let xAxis = getRandomInt(1, 80);
+  let yAxis = getRandomInt(1, 80);
+
+  interface CoOrdinates {
+    x: number;
+    y: number;
+  }
+
+  let cooridinates = {} as CoOrdinates;
+  cooridinates.x = xAxis;
+  cooridinates.y = yAxis;
+
+  return cooridinates;
+}
+
+function addStarToStarscape() {
+  var starPosition = starCoOrdinates();
+  interface Star {
+    style: any;
+  }
+  let newStar = document.createElement('div') as Element & Star;
+  const starId = newStar.id;
+  starmap.set(starId, starPosition);
+  newStar.innerHTML = starGraphic;
+  newStar.className = 'star';
+  newStar.style.left = starPosition.x + `dvw`;
+  newStar.style.top = starPosition.y + `dvh`;
+  starscape.prepend(newStar);
+}
+
+function removeStarFromStarscape() {
+  const star = starscape.querySelector('.star');
+  if (star) {
+    starmap.delete(star.id);
+    starscape.removeChild(star);
+  }
+}
+
+const starNumber = document.querySelector('#star-number') as Element;
+
+function updateStarsCount(numberOfPlayers: number) {
+  var number = numberOfPlayers.toString();
+  if (numberOfPlayers === 1) {
+    starNumber.innerHTML = `is ${number} star`;
+  } else if (numberOfPlayers > 1) {
+    starNumber.innerHTML = `are ${number} stars`;
+  }
+}
+
+let numberOfPlayers: number;
+
+const actor = await actorFromStately(
+  {
+    url: 'https://sky.stately.ai/GK0FU3',
+    sessionId: 'shared-session',
+    onPlayerJoined(info) {
+      numberOfPlayers = info.numberOfPlayers;
+      addStarToStarscape();
+      updateStarsCount(numberOfPlayers);
+    },
+    onPlayerLeft(info) {
+      numberOfPlayers = info.numberOfPlayers;
+      removeStarFromStarscape();
+      updateStarsCount(numberOfPlayers);
+    },
+  },
+  skyConfig,
+);
+
+actor.subscribe((snapshot) => {
+  console.log('Actor changed:', snapshot);
+});
+
+actor.start();
 
 interface SoundButton {
-    value: string;
+  value: string;
 }
 const soundButton = document.querySelector('#sound-toggle') as Element &
   SoundButton;
@@ -47,128 +115,42 @@ function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// Randomise audio tracks
+// // Randomise audio tracks
 
 function randomAudio() {
-    const audioUrlPrefix =
-      'https://ascelcgzufjyvdzuplwo.supabase.co/storage/v1/object/public/demo-assets/laura/';
-    const chooseTrack: number = getRandomInt(1, 3);
-    let track;
-    if (chooseTrack === 1) {
-      track = 'one.mp3';
-    } else if (chooseTrack === 2) {
-      track = 'two.mp3';
-    } else if (chooseTrack === 3) {
-      track = 'three.mp3';
-    }
-    audio.src = `${audioUrlPrefix}/${track}`;
+  const audioUrlPrefix =
+    'https://ascelcgzufjyvdzuplwo.supabase.co/storage/v1/object/public/demo-assets/laura';
+  const chooseTrack: number = getRandomInt(1, 3);
+  let track;
+  if (chooseTrack === 1) {
+    track = 'one.mp3';
+  } else if (chooseTrack === 2) {
+    track = 'two.mp3';
+  } else if (chooseTrack === 3) {
+    track = 'three.mp3';
+  }
+  audio.src = `${audioUrlPrefix}/${track}`;
+  console.log('Randomised audio', audio.src);
 }
 
+randomAudio();
+audio.load();
+
 function updateSoundButtonText(state: any) {
-    const soundOn = state.matches({
-      Sound: 'On',
-    });
-    if (soundOn === true) {
-      soundButton.innerHTML = 'Turn sound off';
-    } else {
-      soundButton.innerHTML = 'Turn sound on';
-    }
-  }
-  
-
-// Add stars
-
-function starCoOrdinates() {
-    let xAxis = getRandomInt(1, 90);
-    let yAxis = getRandomInt(1, 90);
-  
-    interface CoOrdinates {
-      x: number;
-      y: number;
-    }
-  
-    let cooridinates = {} as CoOrdinates;
-    cooridinates.x = xAxis;
-    cooridinates.y = yAxis;
-  
-    return cooridinates;
-  }
-  
-  function addStarToStarscape() {
-    var starPosition = starCoOrdinates();
-    interface Star {
-      style: any;
-    }
-    let newStar = document.createElement('div') as Element & Star;
-    newStar.innerHTML = starGraphic;
-    newStar.className = 'star';
-    newStar.style.left = starPosition.x + `vw`;
-    newStar.style.top = starPosition.y + `vh`;
-    starscape.prepend(newStar);
-  }
-
-  function updateNumberofStars() {
-    // TODO
-    // starNumber.innerHTML = numberOfStarsAsText;
-  }
-
-  function createSession() {
-    // TODO
-  }
-
-  function removeSession() {
-    // TODO
-  }
-  
-  function addStar() {
-    addStarToStarscape();
-  }
-  
-  function removeStar() {
-  
-  }
-
-  const sleepMachine = createActor(
-    actor.provide({
-      actions: {
-        createSession: ({}) => {
-          createSession();
-        },
-        addStar: ({}) => {
-          addStar();
-        },
-        removeSession: ({}) => {
-          removeSession();
-        },
-        removeStar: ({}) => {
-          removeStar();
-        },
-        randomAudio: ({}) => {
-          randomAudio();
-          audio.load();
-        },
-      },
-      actors: {
-        audioPlayer: fromCallback(({ receive }) => {
-          receive((event: any) => {
-            if (event.type === 'play') {
-              audio.play();
-            } else if (event.type === 'pause') {
-              audio.pause();
-            }
-          });
-        }),
-      },
-    }),
-  );
-  
-
-  sleepMachine.subscribe((state) => {
-    updateSoundButtonText(state);
+  const soundOn = state.matches({
+    Sound: 'On',
   });
-
-  sleepMachine.start();
+  if (soundOn === true) {
+    soundButton.innerHTML = 'Turn sound off';
+    audio.play();
+  } else {
+    soundButton.innerHTML = 'Turn sound on';
+    audio.pause();
+  }
+}
 
 soundButton?.addEventListener('click', () => {
-  sleepMachine.send({ type: 'toggle sound' });
+  actor.send({ type: 'toggle sound' });
+
+  updateSoundButtonText(actor.getSnapshot());
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   // We need this here to ensure changes to the .env file are picked up while running in dev mode
-  optimizeDeps: { include: ['@statelyai/sky', '@statelyai/sky-react'] },
+  optimizeDeps: { include: ['@statelyai/sky'] },
 });
